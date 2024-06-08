7ffd4d69f558d35605eda1cbeb4beb0faecca953
rahul-sharma13 <rahulharit13@gmail.com>
2024-06-08 17:52:19 +0530

    chore: Add Hero component to home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
 // layout.tsx is responsible for our font. here we are using inter
-const inter = Inter({ subsets: ["latin"] });
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  variable:"--font-jetbrainsMono"
+});
 
 export const metadata: Metadata = {
   title: "Rahul's Portfolio",
@@ -17,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={jetbrainsMono.variable}>{children}</body>
     </html>
   );
 }
