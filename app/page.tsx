9a5fda07c8f42aef9d63eb5700f3a0a8e9384ba6
rahul-sharma13 <rahulharit13@gmail.com>
2024-06-13 15:52:34 +0530

    minor bug fix

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ const Home = () => {
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">
-              Hello I'm <br /> 
+              Hello I&apos;m <br /> 
               <span className="text-portColor">Rahul Sharma</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,7 +1,38 @@
+"use client";
+
+import { BsArrowDownRight } from "react-icons/bs";
+import Link from "next/link";
+import { motion } from "framer-motion";
+
+const services: { num: string; title: string; description: string }[] = [
+  {
+    num: "01",
+    title: "Web Development",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. ",
+  },
+  {
+    num: "01",
+    title: "Web Development",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. ",
+  },
+  {
+    num: "01",
+    title: "Web Development",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. ",
+  },
+  {
+    num: "01",
+    title: "Web Development",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. ",
+  },
+];
+
 const Services = () => {
   return (
-    <div>Service page</div>
-  )
-}
+    <section>
+      <div className="container mx-auto">services page</div>
+    </section>
+  );
+};
 
-export default Services
+export default Services;
