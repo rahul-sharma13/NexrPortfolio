b1d8dd79a0b3c342376fd41f887bff3cc9a99f63
rahul-sharma13 <rahulharit13@gmail.com>
2024-06-15 17:09:14 +0530

    bug fix

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,15 +3,6 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectTrigger,
-  SelectLabel,
-  SelectItem,
-  SelectValue,
-} from "@/components/ui/select";
 
 import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
 import { motion } from "framer-motion";
@@ -44,7 +35,7 @@ const Contact = () => {
           {/* form */}
           <div className="xl:h-[54%] order-2 xl:order-none">
             <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-              <h3 className="text-4xl text-portColor">Let's work together</h3>
+              <h3 className="text-4xl text-portColor">Let&apos;s work together</h3>
               <p className="text-white/60">Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
               {/* input */}
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
